6fdd71fc2b92885b5b103063eabfaeaedba30f90
Endersky78 <jackhart591@gmail.com>
2023-01-27 17:04:06 -0800

    Got web socket working, started JSON deserialization

---
+++ Assets/Scripts/InputTree.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTree : MonoBehaviour {
+    public BoundingBox boundingBox;
+    public int Age;
+    public string Species;
+}
--- Assets/Scripts/Socket.cs
+++ Assets/Scripts/Socket.cs
@@ -1,18 +1,64 @@
+using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading;
 using System.Collections;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Socket : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class Socket : MonoBehaviour {
+
+    static ClientWebSocket socket;
+
+    Task Connected;
+    bool specified;
+
+    private void Start() {
         
+        socket = new ClientWebSocket();
+        specified = false;
+
+        Connected = socket.ConnectAsync(new Uri("ws://localhost:8000"), CancellationToken.None);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private async void Update() {
+        if (Connected.IsCompleted && !specified) {
+            Debug.Log("Connection Complete!");
+            specified = true;
+            await socket.SendAsync(Encoding.UTF8.GetBytes("R"), WebSocketMessageType.Text, true, CancellationToken.None);
+        } else if (specified) {
+            await GetNewTrees();           
+        }
+    }
+
+    private async Task GetNewTrees() {
+        ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[2048]);
+        string tree = await Receive();
+
+        InputTree inputTree = JsonConvert.DeserializeObject<InputTree>(tree);
+
+        Debug.Log($"Species: {inputTree.Species} Age: {inputTree.Age} Center: {inputTree.boundingBox.Center}");
+    }
+
+    private async Task<string> Receive() {
+        ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[2048]);
+        string recvStr;
+
+        WebSocketReceiveResult result;
+        using (MemoryStream ms = new MemoryStream()) {
+            do {
+                result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+                ms.Write(buffer.Array, buffer.Offset, result.Count);
+            } while (!result.EndOfMessage);
+
+            ms.Seek(0, SeekOrigin.Begin);
+            using (StreamReader reader = new StreamReader(ms, Encoding.UTF8))
+                recvStr = await reader.ReadToEndAsync();
+        }
+
+        return recvStr;
     }
 }
