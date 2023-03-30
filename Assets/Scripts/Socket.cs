dcd330918aeab387d5224be7208134863affce42
Samuel Rundel <samuel.rundel@live.de>
2023-03-30 11:14:52 -0700

    TCP is fixed

--- Assets/Scripts/Socket.cs
+++ Assets/Scripts/Socket.cs
@@ -24,6 +24,8 @@ public class Socket : MonoBehaviour {
 
 
     public BoundingBoxRenderer BBrenderer;
+    private bool connected;
+
     private void Start() {
         
         socket = new ClientWebSocket();
@@ -31,52 +33,72 @@ public class Socket : MonoBehaviour {
 
         // to connect, make sure to use computer's current IP address, otherwise hololens build
         // will not connect
-        Connected = socket.ConnectAsync(new Uri($"ws://{IP}:7000"), CancellationToken.None);
+        try
+        {
+            Connected = socket.ConnectAsync(new Uri($"ws://{IP}:7000"), CancellationToken.None);
+            Connected.Wait();
+            connected = true;
+        }
+        catch (Exception ex)
+        {
+            connected = false;
+            Debug.LogException(ex);
+        }
+        
     }
 
     private async void Update() {
+        //cube.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
         try
         {
-            if (!specified)
+            if (!connected)
             {
-                string message = "Connection Complete!";
-                Debug.Log(message);
-                specified = true;
-                await socket.SendAsync(Encoding.UTF8.GetBytes("R"), WebSocketMessageType.Text, true, CancellationToken.None);
-                //Orange
-                cube.GetComponent<Renderer>().material.color = new Color(0, 0, 255);
+                Debug.Log("Initial Connection failed. Won't be able to retrieve any data");
             }
-            else if (specified)
+            else
             {
-                if (!recieving)
+                if (!specified)
                 {
-                    Debug.Log("Retrieving values");
-                    string newTree = await Receive();
-                    if (newTree.Substring(0, 1) == "C")
-                        GetNewTrees(newTree.Substring(6));
-                    else if (newTree.Substring(0, 1) == "U")
-                        UpdateNewTrees(newTree.Substring(6));
-                    else if (newTree.Substring(0, 1) == "R")
+                    await socket.SendAsync(Encoding.UTF8.GetBytes("R"), WebSocketMessageType.Text, true, CancellationToken.None);
+                    specified = true;
+                    string message = "Connection Complete!";
+                    Debug.Log(message);
+                    //Orange
+                    cube.GetComponent<Renderer>().material.color = new Color(0, 0, 255);
+                }
+                else if (specified)
+                {
+                    if (!recieving)
                     {
-                        BBrenderer.ClearTrees(); // Can call this because remove and add trees are done as separate events
-                        RemoveTrees(newTree.Substring(6));
+                        Debug.Log("Retrieving values");
+                        string newTree = await Receive();
+                        if (newTree.Substring(0, 1) == "C")
+                            GetNewTrees(newTree.Substring(6));
+                        else if (newTree.Substring(0, 1) == "U")
+                            UpdateNewTrees(newTree.Substring(6));
+                        else if (newTree.Substring(0, 1) == "R")
+                        {
+                            BBrenderer.ClearTrees(); // Can call this because remove and add trees are done as separate events
+                            RemoveTrees(newTree.Substring(6));
+                        }
+                        else if (newTree.Substring(0, 1) == "L")
+                        {
+                            UpdateTrees(newTree.Substring(6));
+                        }
+                        //GREEN
+                        cube.GetComponent<Renderer>().material.color = new Color(124, 252, 0);
                     }
-                    else if(newTree.Substring(0, 1) == "L")
-                    {
-                        UpdateTrees(newTree.Substring(6));
-                    }
-                    //GREEN
-                    cube.GetComponent<Renderer>().material.color = new Color(124, 252, 0);
                 }
             }
         }
         catch (Exception ex)
         {
             //RED
-            cube.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
             connectionAttempt++;
+            specified = false;
+            cube.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
             Debug.Log($"#\t{ex.Message}");
-            Debug.Log($"#\tAttempted Connection {connectionAttempt}");
+            Debug.Log($"#\tConnection failed ({connectionAttempt})");
         }
         
     }
@@ -119,20 +141,30 @@ public class Socket : MonoBehaviour {
     }
 
     private async Task<string> Receive() {
+        
         ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[2048]);
-        string recvStr;
+        string recvStr = "";
 
         WebSocketReceiveResult result;
         recieving = true;
-        using (MemoryStream ms = new MemoryStream()) {
-            do {
-                result = await socket.ReceiveAsync(buffer, CancellationToken.None);
-                ms.Write(buffer.Array, buffer.Offset, result.Count);
-            } while (!result.EndOfMessage);
+        try
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                do
+                {
+                    result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+                    ms.Write(buffer.Array, buffer.Offset, result.Count);
+                } while (!result.EndOfMessage);
 
-            ms.Seek(0, SeekOrigin.Begin);
-            using (StreamReader reader = new StreamReader(ms, Encoding.UTF8))
-                recvStr = await reader.ReadToEndAsync();
+                ms.Seek(0, SeekOrigin.Begin);
+                using (StreamReader reader = new StreamReader(ms, Encoding.UTF8))
+                    recvStr = await reader.ReadToEndAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError(ex);
         }
 
         recieving = false;
